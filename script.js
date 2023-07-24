8625f281d6f01288b16d79952a9b42183d862516
Charlie <131995134+weeklyrambler@users.noreply.github.com>
2023-07-24 15:21:52 +0100

    added scores function, score reset, game is complete for now but will come back to add proper loop and player interface in the future.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+let playerScore = 0;
+let computerScore = 0; 
+
 function getComputerChoice() {
   let num =  Math.floor(Math.random() * 3); 
   let item = null; 
@@ -13,39 +16,136 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let player = playerSelection.toLowerCase();  
+  let player = playerSelection; 
   console.log(player); 
   computerSelection = getComputerChoice(); 
-  console.log(computerSelection); 
-  let outcome = null; 
+  console.log(computerSelection);  
+  let outcome = ' '; 
   if (player == 'rock' && computerSelection == 'paper') {
    outcome = "Oh no! The computer chose paper, you lose!"; 
+   computerScore += 1; 
   } else if (player == 'rock' && computerSelection == 'scissors') {
-   outcome = "Yes! rock beats scissors! You win!"; 
+   outcome = "Yes! rock beats scissors! You win!";  
+   playerScore += 1; 
   } else if (player == 'rock' && computerSelection == 'rock') {
    outcome = "Yikes! It's a tie, you both chose rock! Try again."; 
   };
 
   if (player == 'paper' && computerSelection == 'scissors') {
-    outcome = "Oh no! The computer chose scissors, you lose!"; 
+    outcome = "Oh no! The computer chose scissors, you lose!";
+    computerScore += 1; 
   } else if (player == 'paper' && computerSelection == 'rock') {
     outcome = "Yes! paper beats rock! You win!";
+    playerScore += 1; 
   } else if (player == 'paper' && computerSelection == 'paper') {
     outcome = "Yikes! It's a tie, you both chose paper! Try again."; 
   };
    
   if (player == 'scissors' && computerSelection == 'rock') {
     outcome = "Oh no! The computer chose rock, you lose!"; 
+    computerScore += 1; 
   } else if (player == 'scissors' && computerSelection == 'paper') {
-    outcome = "Yes! scissors beats paper! You win!"; 
+    outcome = "Yes! scissors beats paper! You win!";  
+    playerScore += 1; 
   } else if (player == 'scissors' && computerSelection == 'scissors') {
     outcome = "Yikes! It's a tie, you both chose scissors! Try again."; 
   }; 
-console.log(outcome); 
-  return outcome; 
+
+  console.log(outcome);  
 
 }
 
-function game() {
-    
+
+function game(answer) {
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);  
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+ 
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+
+ answer = prompt("Please enter rock, paper, or scissors:",)
+ playRound(answer.toLowerCase()); 
+ console.log("Your score is: " + playerScore);
+ console.log("The computer's score is: " + computerScore);
+ endGame();
+}
+
+game(); 
+
+function endGame() {
+    if (computerScore == 5) {
+        console.log("Uh oh! The computer has reached 5 points and won the game. Better luck next time.")
+        computerScore = 0; 
+        playerScore = 0; 
+     } else if (playerScore == 5) {
+        console.log("Yes! You have reached 5 points and have won the game! Congratulations!")
+        computerScore = 0; 
+        playerScore = 0; 
+     }
 }
